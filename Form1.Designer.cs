be713972e97004e0695a505b4680552da0588ea3
NArutoUzUmak190 <103091946+NArutoUzUmak190@users.noreply.github.com>
2022-11-21 19:07:25 -0600

    Se agrego

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace Kruskal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B = new System.Windows.Forms.PictureBox();
+            this.D = new System.Windows.Forms.PictureBox();
+            this.S = new System.Windows.Forms.PictureBox();
+            this.C = new System.Windows.Forms.PictureBox();
+            this.E = new System.Windows.Forms.PictureBox();
+            this.T = new System.Windows.Forms.PictureBox();
+            this.Tabla = new System.Windows.Forms.Panel();
+            this.Prueba = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.B)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.D)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.S)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.E)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.T)).BeginInit();
+            this.Tabla.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // B
+            // 
+            this.B.Image = global::Kruskal.Properties.Resources.c;
+            this.B.Location = new System.Drawing.Point(135, 84);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(42, 38);
+            this.B.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.B.TabIndex = 0;
+            this.B.TabStop = false;
+            // 
+            // D
+            // 
+            this.D.Image = global::Kruskal.Properties.Resources.c;
+            this.D.Location = new System.Drawing.Point(291, 84);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(42, 38);
+            this.D.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.D.TabIndex = 1;
+            this.D.TabStop = false;
+            // 
+            // S
+            // 
+            this.S.Image = global::Kruskal.Properties.Resources.c;
+            this.S.Location = new System.Drawing.Point(33, 186);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(42, 38);
+            this.S.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.S.TabIndex = 2;
+            this.S.TabStop = false;
+            // 
+            // C
+            // 
+            this.C.Image = global::Kruskal.Properties.Resources.c;
+            this.C.Location = new System.Drawing.Point(135, 275);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(42, 38);
+            this.C.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.C.TabIndex = 3;
+            this.C.TabStop = false;
+            // 
+            // E
+            // 
+            this.E.Image = global::Kruskal.Properties.Resources.c;
+            this.E.Location = new System.Drawing.Point(291, 275);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(42, 38);
+            this.E.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.E.TabIndex = 4;
+            this.E.TabStop = false;
+            // 
+            // T
+            // 
+            this.T.Image = global::Kruskal.Properties.Resources.c;
+            this.T.Location = new System.Drawing.Point(403, 186);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(42, 38);
+            this.T.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.T.TabIndex = 5;
+            this.T.TabStop = false;
+            // 
+            // Tabla
+            // 
+            this.Tabla.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Tabla.Controls.Add(this.textBox9);
+            this.Tabla.Controls.Add(this.textBox8);
+            this.Tabla.Controls.Add(this.textBox7);
+            this.Tabla.Controls.Add(this.textBox6);
+            this.Tabla.Controls.Add(this.textBox5);
+            this.Tabla.Controls.Add(this.textBox4);
+            this.Tabla.Controls.Add(this.textBox3);
+            this.Tabla.Controls.Add(this.textBox2);
+            this.Tabla.Controls.Add(this.textBox1);
+            this.Tabla.Controls.Add(this.B);
+            this.Tabla.Controls.Add(this.T);
+            this.Tabla.Controls.Add(this.D);
+            this.Tabla.Controls.Add(this.E);
+            this.Tabla.Controls.Add(this.S);
+            this.Tabla.Controls.Add(this.C);
+            this.Tabla.Location = new System.Drawing.Point(423, 47);
+            this.Tabla.Name = "Tabla";
+            this.Tabla.Size = new System.Drawing.Size(500, 400);
+            this.Tabla.TabIndex = 6;
+            // 
+            // Prueba
+            // 
+            this.Prueba.Location = new System.Drawing.Point(254, 56);
+            this.Prueba.Name = "Prueba";
+            this.Prueba.Size = new System.Drawing.Size(75, 23);
+            this.Prueba.TabIndex = 7;
+            this.Prueba.Text = "button1";
+            this.Prueba.UseVisualStyleBackColor = true;
+            this.Prueba.Click += new System.EventHandler(this.Prueba_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(120, 186);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(31, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(220, 74);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(31, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(220, 204);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(31, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(220, 304);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(31, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(317, 186);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(31, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(387, 130);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(31, 20);
+            this.textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(387, 257);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(31, 20);
+            this.textBox9.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 501);
+            this.Controls.Add(this.Prueba);
+            this.Controls.Add(this.Tabla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.B)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.D)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.S)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.E)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.T)).EndInit();
+            this.Tabla.ResumeLayout(false);
+            this.Tabla.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox B;
+        private System.Windows.Forms.PictureBox D;
+        private System.Windows.Forms.PictureBox S;
+        private System.Windows.Forms.PictureBox C;
+        private System.Windows.Forms.PictureBox E;
+        private System.Windows.Forms.PictureBox T;
+        private System.Windows.Forms.Panel Tabla;
+        private System.Windows.Forms.Button Prueba;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kruskal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Pen Rojo = new Pen(Color.Red,3);
+        Pen Negro = new Pen(Color.DarkBlue, 3);
+
+        public void esqueleto()
+        {
+            Graphics grafica = Tabla.CreateGraphics();
+
+            grafica.DrawLine(Negro, 50, 200, 152, 98);//S - B
+            grafica.DrawLine(Negro, 50, 200, 152, 289);//S - C
+            grafica.DrawLine(Negro, 152, 98, 308, 98);//B - D
+            grafica.DrawLine(Negro,152, 98, 152,289);//B - C
+            grafica.DrawLine(Negro, 152, 289, 308, 98);//C - D
+            grafica.DrawLine(Negro, 152, 289, 308, 289);//C - E
+            grafica.DrawLine(Negro, 308, 98, 420, 200);//D - T
+            grafica.DrawLine(Negro, 308, 98, 308, 289);//D - E
+            grafica.DrawLine(Negro,308,289,420,200);//E - T
+        }
+
+        private void Prueba_Click(object sender, EventArgs e)
+        {
+            esqueleto();
+        }
+    }
+}
